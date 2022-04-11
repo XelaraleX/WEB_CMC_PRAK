14372f794d87bdc7f581c0dfa8998b7a478c5b19
Alexander Romanov <xellar@ispras.ru>
2022-04-11 21:53:41 +0300

    Done AircraftDAOTest.java

--- src/test/java/ru/msu/cmc/webprak/model/AircraftDAOTest.java
+++ src/test/java/ru/msu/cmc/webprak/model/AircraftDAOTest.java
@@ -21,6 +21,52 @@ public class AircraftDAOTest {
         this.runId = UUID.randomUUID().toString();
 
         this.aircraftTest1 = new Aircraft();
+        this.aircraftTest1.setModelName("TestName1" + this.runId);
 
+        this.aircraftTest2 = new Aircraft();
+        this.aircraftTest2.setModelName("TestName2" + this.runId);
+
+        this.dao.add(aircraftTest1);
+        this.dao.add(aircraftTest2);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        this.dao.delete(this.aircraftTest1);
+        this.dao.delete(this.aircraftTest2);
+
+        this.dao = null;
+        this.runId = null;
+        this.aircraftTest1 = null;
+        this.aircraftTest2 = null;
+    }
+
+    @Test
+    public void testGetByName() {
+        Collection<Aircraft> allTestNames = this.dao.getAircraftByFilter(
+                AircraftDAO.getFilterBuilder()
+                        .modelName(this.runId)
+                        .build()
+        );
+        Set<Aircraft> expected = new HashSet<>();
+        expected.add(this.aircraftTest1);
+        expected.add(this.aircraftTest2);
+
+        Set<Aircraft> got = new HashSet<>(allTestNames);
+
+        assertEquals(expected, got);
+
+        Collection<Aircraft> onlyTest1 = this.dao.getAircraftByFilter(
+                AircraftDAO.getFilterBuilder()
+                        .modelName("TestName1" + this.runId)
+                        .build()
+        );
+
+        expected = new HashSet<>();
+        expected.add(this.aircraftTest1);
+
+        got = new HashSet<>(onlyTest1);
+
+        assertEquals(expected, got);
     }
 }
